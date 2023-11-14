dc4871c036a59c19df1c6953d28ac26113a01acf
Koharu Fujimura <asukuko.haru.119@gmail.com>
2023-11-14 21:39:48 +0900

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import "@/app/ui/global.css";
+import { notoSansJP } from "@/app/ui/fonts";
+
 export default function RootLayout({
+  // 一番根本
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${notoSansJP.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Noto_Sans_JP, Lusitana } from "next/font/google";
+
+export const notoSansJP = Noto_Sans_JP({ subsets: ["latin"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
